c8c22a5a59b905fa1b71fff25c721269604289c5
kimberleymanzano <69652425+kimberleymanzano@users.noreply.github.com>
2022-06-06 12:26:00 -0700

    carrito v3

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service'
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor(public productservice: ProductsService) { }
 
   ngOnInit(): void {
+    console.log(this.productservice.getCart())
   }
 
 }
