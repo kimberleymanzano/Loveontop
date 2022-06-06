fc9e7c9b2c5e7d6f5fbb64816c192a343bf016a4
kimberleymanzano <69652425+kimberleymanzano@users.noreply.github.com>
2022-06-06 00:37:34 -0700

    Contact-us ready

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import* as M from 'materialize-css';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(){
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.collapsible');
+      var instances = M.Collapsible.init(elems, { });
+    });
   }
 
 }
