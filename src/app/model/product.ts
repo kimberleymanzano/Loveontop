40d4af7851d6fb9b357e7eb2970643c6405cedc8
kimberleymanzano <69652425+kimberleymanzano@users.noreply.github.com>
2022-06-05 17:56:45 -0700

    ay ya no se

---
+++ src/app/model/product.ts
@@ -0,0 +1,14 @@
+export interface Producto{
+    id: string
+    title: string
+    brand: string
+    category: string
+    price: string
+    img: string
+    sale: string
+    sale_price: string
+    service: string
+    recommended: string
+    popular: string
+
+}
